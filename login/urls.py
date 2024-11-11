0398a3fb2f4f9595b868e80e7c1f48c8860b3074
avneeshlingwal9 <avneeshlingwal@gmail.com>
2024-11-11 17:36:03 +0530

    Added a database and a submit button for user info

--- login/urls.py
+++ login/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from login import views 
 
 urlpatterns = [
-    path("", views.index,name="home")
+    path('', views.index,name='home'),
+    path('login', views.login_page,name='loginuser')
 ]
--- login/views.py
+++ login/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import UserApp
 
 # Create your views here.
 def index(request):
+    return HttpResponse("Hello")
+def login_page(request):
+    if(request.method == "POST"):
+        name = request.POST.get('username')
+        passwrd = request.POST.get('password')
+        user = UserApp(name=name, passwrd=passwrd)
+        user.save()
     return render(request, 'login.html')
