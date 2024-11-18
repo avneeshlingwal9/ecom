fb1c28cb326a58bbf67fd4cf14bd306ac0f375e0
avneeshlingwal9 <avneeshlingwal@gmail.com>
2024-11-18 19:31:18 +0530

    The order is now accessible to authorized users only and orders are not saving currently

--- login/urls.py
+++ login/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-
+from django.contrib.auth.views import LogoutView
 from login import views 
 
 
 app_name = 'login'
 urlpatterns = [
-    path('', views.login_page,name='loginPage'),
+    path('login/', views.login_page,name='loginPage'),
     path('register/',views.register_user, name='registerUser'),
     path('orders/', views.orders, name = 'orders'), 
-    path('product/', views.product_insertion, name= 'product')
+    path('product/', views.product_insertion, name= 'product'),
+    path('logOut/' ,LogoutView.as_view(next_page='login:loginPage'), name= 'logOut' )
 ]
 
