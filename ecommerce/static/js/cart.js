a4388d098164f483827e332d7ba1ecbb937945f8
avneeshlingwal9 <avneeshlingwal@gmail.com>
2025-05-26 10:02:05 +0530

    Add cart

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -1,1 +1,47 @@
-console.log("Hellow World")
+
+
+var updateBtn = document.getElementsByClassName('update-cart')
+
+console.log(updateBtn.length)
+
+for (i = 0; i < updateBtn.length; i++) {
+    updateBtn[i].addEventListener('click', function () {
+        
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log('productId:', productId, 'Action:', action)
+        console.log('User', user)
+        if (user == 'AnonymousUser') {
+            console.log("User is not authenticated.")
+        }
+        else {
+            updateUser(productId , action)
+        }
+    })
+}
+
+function updateUser(productId , action) {
+
+    const url = '/update_items/'
+    var data = {
+        'productId': productId,
+        'action': action
+    }
+    var fetchData = {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: new Headers({ 'Content-Type': 'application/json', 'X-CSRFToken' : csrftoken})
+        
+
+    }
+    fetch(url, fetchData)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            location.reload()
+            console.log('DATA:', data)
+        }
+        );
+
+}
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 
+from django.http import JsonResponse
 from .models import *
+import json
 # Create your views here.
 def store(request):
     products = Product.objects.all()
-    context = {'products' : products}
 
+    if request.user.is_authenticated:
+
+        customer = request.user.customer
+        orders , created = Order.objects.get_or_create(customer = customer , complete = False)
+        cartItems = orders.order_quantity
+    else:
+
+        orders = {'order_quantity' : 0}
+        cartItems = orders['order_quantity']
+
+    context = {'products': products , 'cart' : cartItems}
+    
     return render(request , 'store/store.html' , context)
 
 def cart(request):
@@ -16,13 +29,15 @@ def cart(request):
         orders, created = Order.objects.get_or_create(customer = customer , complete = False)
 
         items = orders.orderitem_set.all()
+        cartItems = orders.order_quantity
 
     else:
 
         items = []
         orders = {'order_quantity': 0 , 'order_total': 0}
+        cartItems = orders['order_quantity']
 
-    context  = {'items': items , 'orders' : orders}
+    context  = {'items': items , 'orders' : orders , 'cart': cartItems}
 
 
 
@@ -39,11 +54,34 @@ def checkout(request):
         orders, created = Order.objects.get_or_create(customer = customer , complete = False)
 
         items = orders.orderitem_set.all()
+        cartItems = orders.order_quantity
     else:
 
         items = []
         orders = {'order_quantity' : 0 , 'order_total': 0}
+        cartItems = orders['order_quantity']
     
-    context = {'items': items , 'orders': orders}
+    context = {'items': items , 'orders': orders , 'cart' : cartItems}
 
     return render(request, 'store/checkout.html' , context)
+
+def update_items(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    customer = request.user.customer
+    product = Product.objects.get(id = productId)
+
+    order, created = Order.objects.get_or_create(customer = customer , complete = False)
+
+    orderitem , created = OrderItem.objects.get_or_create(order = order , product = product)
+
+    if action == "add":
+        orderitem.quantity = orderitem.quantity + 1 
+    elif action == "remove":
+        orderitem.quantity = orderitem.quantity - 1 
+
+    if orderitem.quantity > 0:
+        orderitem.save()
+
+    return JsonResponse('Item was added' , safe=False)
